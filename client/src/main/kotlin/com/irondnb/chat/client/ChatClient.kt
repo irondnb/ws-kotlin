2d3b4c8de9ca0631247d4266c08c85490450a4e2
Michal Szota <m.szota@vasco-electronics.com>
2021-09-16 00:59:50 +0200

    Implement Chat client

--- client/src/main/kotlin/com/irondnb/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/irondnb/chat/client/ChatClient.kt
@@ -6,8 +6,48 @@ import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
 import io.ktor.util.*
 import kotlinx.coroutines.*
+import java.lang.Exception
 
-@KtorExperimentalAPI
+suspend fun DefaultClientWebSocketSession.outputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            val receivedText = message.readText()
+            println(receivedText)
+        }
+    } catch (e: Exception) {
+        println("Error while receiving: ${e.localizedMessage}")
+    }
+}
+
+suspend fun DefaultClientWebSocketSession.inputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if (message.equals("exit", true)) return
+        try {
+            send(message)
+        } catch (e: Exception) {
+            println("Error while sending: " + e.localizedMessage)
+            return
+        }
+    }
+}
+
 fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "0.0.0.0", port = 8080, path = "/chat") {
+            while (true) {
+                val messageOutputRoutine = launch { outputMessages() }
+                val userInputRoutine = launch { inputMessages() }
 
+                userInputRoutine.join() // Wait for completion; either "exit" or error
+                messageOutputRoutine.cancelAndJoin()
+            }
+        }
+    }
+    client.close()
+    println("Bye!<3")
 }
