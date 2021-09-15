0f8385391c2f9fc4a24607d377cb411c7735d792
Michal Szota <m.szota@vasco-electronics.com>
2021-09-15 16:58:00 +0200

    Add dependencies

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -18,9 +18,6 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
 }
